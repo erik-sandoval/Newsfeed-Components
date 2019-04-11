227b62295e92d398bf5448f97bbe09f3ca39a49a
erik-sandoval <erik.sandoval@icloud.com>
2019-04-10 23:07:06 -0700

    added more animations

--- Animations/Animations.js
+++ Animations/Animations.js
@@ -1,16 +1,22 @@
+const menu1 = document.querySelector(".menu-button")
+const menu2 = document.querySelector('.menu')
 const img = document.querySelector('.menu-button')
-const header = document.querySelector('.header')
-const article = document.querySelectorAll('.article')
+const expBtn = document.querySelectorAll('.expandButton')
 
 
+TweenMax.from(".header h1", 3, { y: "-120", opacity: 0, ease: Back.easeOut.config(1) })
 
-TweenMax.to(header, 1, {backgroundColor: 'red', repeat: 1, yoyo: true})
+TweenMax.from(".header", 2, { backgroundColor: 'dodgerblue', repeat: 0 })
 
-img.addEventListener('mouseover', event =>{
-    TweenMax.to(img, 1, { rotation: "360", repeat: 1, transformOrigin: "50% 50%" })
+img.addEventListener('mouseover', () => {
+    TweenMax.to(menu1, 1, { rotation: "360", repeat: 0, transformOrigin: "50% 50%" });
+});
+
+img.addEventListener('mouseleave', () => {
+    TweenMax.to(menu1, 1, { rotation: "0", repeat: 0, transformOrigin: "50% 50%" });
 })
 
+menu1.addEventListener('click', () => {
+    TweenMax.from('.menu--open', 2, { x: "-100%", ease: Power1.easeOut })
 
-
-
-TweenMax.to(article, 0.1, {x: "+=20", yoyo: true, repeat: 5 });
+})
